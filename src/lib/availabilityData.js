b5b43d0c6a50360722f019e37ab3d2f944482c5e
rogerbf <roger@bergfalk.nu>
2016-08-07 10:14:16 +0200

    log url

--- src/lib/availabilityData.js
+++ src/lib/availabilityData.js
@@ -6,6 +6,7 @@ const URL = 'http://www.bookatable.se/bookingflow/availability'
 const getAvailabilityData = id => {
   const date = moment().add(1, 'd').format('YYYY-MM-DD')
   const query = `?covers=2&date=${date}&time=19:00&salesforceid=${id}&language=sv-SE`
+  console.log(`GET: ${URL}${query}`)
   return request(`${URL}${query}`)
 }
 
